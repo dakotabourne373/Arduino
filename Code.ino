24b3735d88699eb5a5ef5ee26331f4126118e0c6
dakotabourne373 <44448765+dakotabourne373@users.noreply.github.com>
2018-10-24 16:46:10 -0400

    Add files via upload

---
+++ Code.ino
@@ -0,0 +1,80 @@
+int switchState = 0;
+int dice = 0;
+void setup() {
+  // put your setup code here, to run once:
+pinMode(2, INPUT);
+pinMode(3, OUTPUT);
+pinMode(4, OUTPUT);
+pinMode(5, OUTPUT);
+pinMode(6, OUTPUT);
+pinMode(7, OUTPUT);
+pinMode(8, OUTPUT);
+pinMode(9, OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+switchState = digitalRead(2);
+if(switchState == HIGH){
+dice = random(1,7);
+
+
+if(dice == 1){
+  digitalWrite(6, HIGH);
+  digitalWrite(3, LOW);
+  digitalWrite(4, LOW);
+  digitalWrite(5, LOW);
+  digitalWrite(7, LOW);
+  digitalWrite(8, LOW);
+  digitalWrite(9, LOW);
+}else if(dice == 2){
+  digitalWrite(3, HIGH);
+  digitalWrite(4, LOW);
+  digitalWrite(5, LOW);
+  digitalWrite(6, LOW);
+  digitalWrite(7, HIGH);
+  digitalWrite(8, LOW);
+  digitalWrite(9, LOW);
+}else if(dice == 3){
+  digitalWrite(3, LOW);
+  digitalWrite(4, LOW);
+  digitalWrite(5, HIGH);
+  digitalWrite(6, HIGH);
+  digitalWrite(7, LOW);
+  digitalWrite(8, LOW);
+  digitalWrite(9, HIGH);
+}else if(dice == 4){
+  digitalWrite(3, HIGH);
+  digitalWrite(4, LOW);
+  digitalWrite(5, HIGH);
+  digitalWrite(6, LOW);
+  digitalWrite(7, HIGH);
+  digitalWrite(8, LOW);
+  digitalWrite(9, HIGH);
+}else if(dice == 5){
+  digitalWrite(3, HIGH);
+  digitalWrite(4, LOW);
+  digitalWrite(5, HIGH);
+  digitalWrite(6, HIGH);
+  digitalWrite(7, HIGH);
+  digitalWrite(8, LOW);
+  digitalWrite(9, HIGH);
+}else if(dice == 6){
+  digitalWrite(3, HIGH);
+  digitalWrite(4, HIGH);
+  digitalWrite(5, HIGH);
+  digitalWrite(6, LOW);
+  digitalWrite(7, HIGH);
+  digitalWrite(8, HIGH);
+  digitalWrite(9, HIGH);
+}
+}else if(switchState == LOW){
+  digitalWrite(3, LOW);
+  digitalWrite(4, LOW);
+  digitalWrite(5, LOW);
+  digitalWrite(6, LOW);
+  digitalWrite(7, LOW);
+  digitalWrite(8, LOW);
+  digitalWrite(9, LOW);
+}
+}
---
+++ Dakota.ino
@@ -0,0 +1,45 @@
+#include <Servo.h>
+
+Servo myServo;
+
+int proxPinQ = A0;
+int proxPin2 = A1;
+float proxValQ;
+float proxVal2;
+int angle;
+int pos = 0;
+
+#define DEBUG 1
+void setup() {
+  // put your setup code here, to run once:
+  myServo.attach(9);
+  Serial.begin(9600);
+  pinMode(proxPinQ, INPUT);
+  delay(2000);
+  pinMode(proxPin2, INPUT);
+}
+void loop() {
+  // put your main code here, to run repeatedly:
+  proxValQ = analogRead(proxPinQ);
+  proxVal2 = analogRead(proxPin2);
+  Serial.print(", Q: ");
+  Serial.print(proxValQ);
+  delay(5);
+  Serial.print(", 2: ");
+  Serial.print(proxVal2);
+  delay(500);
+
+  if (proxValQ >= 250) {
+      myServo.write(0);              // tell servo to go to position in variable 'pos'
+    }
+  else if (proxValQ < 250) {
+      myServo.write(90);              // tell servo to go to position in variable 'pos'
+  }
+  if (proxVal2 >= 250) {
+      myServo.write(0);              // tell servo to go to position in variable 'pos'
+    }
+  else if (proxVal2 < 250) {
+      myServo.write(90);              // tell servo to go to position in variable 'pos'
+    }
+}
+
